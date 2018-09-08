d702b3ec55b39d87d9ef9a9fbb37159251e06b3e
Raihan Kabir <iraihankabir@yahoo.com>
2018-09-09 01:20:15 +0600

    edited comments

--- Pattern Recognition/main.py
+++ Pattern Recognition/main.py
@@ -4,12 +4,12 @@ from MyFirstNN import data
 
 if __name__ == "__main__":
 	
-	#Intialise a single neuron neural network.
+	#Intialization of the neural network.
 	nn = Network()
 
-	# train model
+	# train network
 	nn.train(data.training_set_inputs, data.training_set_outputs, no_of_training=100000)
 
-	# test the neural network model against some new data
+	# test the neural network against some new data
 	nn.test(data.test_set_inputs, data.test_set_outputs)
 	
