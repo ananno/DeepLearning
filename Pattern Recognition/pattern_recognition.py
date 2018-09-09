acbc1cc03b0928d50e7206958f565ee4dcbfac69
Sagor <iraihankabir@yahoo.com>
2018-09-09 14:54:17 +0600

    changed names

--- Pattern Recognition/pattern_recognition.py
+++ Pattern Recognition/pattern_recognition.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 	nn = Network()
 
 	# train network
-	nn.train(data.training_set_inputs, data.training_set_outputs, no_of_training=900000)
+	nn.train(data.training_set_inputs, data.training_set_outputs, no_of_training=100000)
 
 	# test the neural network against some new data
 	nn.test(data.test_set_inputs, data.test_set_outputs)
